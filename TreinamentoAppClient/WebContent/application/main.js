fb57938df8121534da0b663145af04818fc5bccb
Renato <rvbmedeiros@gmail.com>
2016-07-06 14:24:07 -0300

    subindo modulos "controle de fluxo e manipulação de erros" e "sintaxes e tipos"

--- TreinamentoAppClient/WebContent/application/main.js
+++ TreinamentoAppClient/WebContent/application/main.js
@@ -1,6 +1,10 @@
-var cao1 = new Cachorro("toto1", 1);
-var cao2 = new Cachorro("toto2", 5);
-var cao3 = new Cachorro("toto3", 10);
-console.log(cao3.teste);
-var canil = new Canil([cao1, cao2, cao3]);
-canil.alimentar();
+try {
+	var cao1 = new Cachorro("toto1", 1);
+	var cao2 = new Cachorro("toto2", 5);
+	var cao3 = new Cachorro("toto3", 10);
+	console.log(cao3.teste);
+	var canil = new Canil([ cao1, cao2, cao3 ]);
+	canil.alimentar();
+} catch (e) {
+	console.log(e);
+}
