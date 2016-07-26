3f94f811d7418c70a8d5bff60429baa8fd1a84cf
Ivan Moleta <imoleta@scopus.com.br>
2016-07-26 12:11:25 -0300

    ajuste

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/controle_de_fluxo_e_manipulacao_de_erro.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/controle_de_fluxo_e_manipulacao_de_erro.js
@@ -2,3 +2,37 @@
  * 
  */
 
+var a = 50;
+
+if(a == 50)
+{
+    try{
+        console.log("Estou aqui");
+    }
+    catch(erro){
+        finally{
+            console.log("Acabou");
+        }
+    }
+}
+else if(a == 12)
+{
+    console.log("Estou aqui")
+}
+else
+{
+    a = 5 + 5;
+}
+
+switch (a > 20)
+        case 1: a = 4-8;
+        break;
+
+        case 2: try{
+                a = 5+8;
+
+        }
+        catch(erro){
+            console.log("Errou")
+        }
+default:    console.log("Estou aqui")
