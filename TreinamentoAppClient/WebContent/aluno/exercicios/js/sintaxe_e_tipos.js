d0aa8c2f5eaed5e38daca7a703a48cea83087584
Ivan Moleta <imoleta@scopus.com.br>
2016-07-26 12:11:56 -0300

    ajuste

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/sintaxe_e_tipos.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/sintaxe_e_tipos.js
@@ -2,3 +2,23 @@
  * 
  */
 
+//Comentário
+
+/*Treinamento
+de
+JavaScript*/
+var 5;
+5 =+ 5;
+alert(5);
+function x
+{
+    var a;
+    var b = [10,20];
+    var c = "oi";
+    let d = "Teste";
+    const e = "Teste";
+    var f = 10;
+    var g = true;
+
+    a = "Teste";
+}
