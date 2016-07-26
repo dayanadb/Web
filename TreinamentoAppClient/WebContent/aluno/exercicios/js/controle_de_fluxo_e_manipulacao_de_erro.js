2cbffd76523d3913681a17ebd54c238889eb13f8
LocalAdm <LocalAdm>
2016-07-26 11:21:47 -0300

    js testando somente if e else

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/controle_de_fluxo_e_manipulacao_de_erro.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/controle_de_fluxo_e_manipulacao_de_erro.js
@@ -1,4 +1,15 @@
-/**
- * 
- */
+function testaIf
+{
+    var c = 10*10;
 
+    if(c == 20)
+    {
+        console.log("Resposta Certa");
+    }
+    else
+    (
+        console.log("Não deu");
+    )
+}
+
+alert(function testaIf());
