9d08955d0c021a4293e7e93f7f2912605a3e1a25
LocalAdm <LocalAdm>
2016-07-26 10:13:29 -0300

    terça20160726

--- TreinamentoAppClient/src/Teste.java
+++ TreinamentoAppClient/src/Teste.java
@@ -10,29 +10,32 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 
-public class Teste {
-
-	public static void main(String[] args) {
+public class Teste
+{
+	public static void main(String[] args)
+	{
 		new Teste().roda();
 	}
 
-	public void roda() {
+	public void roda()
+	{
 		HttpURLConnection conn;
-		try {
+		try
+		{
 			URL url = new URL("http://www.ee.usp.br/biblioteca/whorta/doc/gua.pdf");
 			conn = (HttpURLConnection) url.openConnection();
 			conn.connect();
 			int s = conn.getResponseCode();
-			if (s == 200) {
+			if (s == 200)
+			{
 				InputStream pdf = conn.getInputStream();
 				File f = new File("C:\\Users\\LocalAdm/tese.pdf");
 				f.createNewFile();
 				Files.copy(pdf, f.toPath(), StandardCopyOption.REPLACE_EXISTING);
 			}
-		} catch (IOException e) {
+		} catch (IOException e)
+		{
 			e.printStackTrace();
 		}
-
 	}
-
 }
