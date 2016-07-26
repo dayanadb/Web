6e0c3d7ec1953d6cda5d1968aec62c8536525a75
LocalAdm <LocalAdm>
2016-07-26 09:44:49 -0300

    release

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/sintaxe_e_tipos.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/sintaxe_e_tipos.js
@@ -1,4 +1,14 @@
 /**
- * 
+ Teste de realização de comentário de bloco
  */
+// declaração de variáveis
+var $a = 10;
+var $b = "teste";
+var $c = [10, "teste", false];
 
+function teste() {
+    var x = "testes";
+    let y = "var de bloco";
+    let z = true;
+    let x = 10.10;
+}
