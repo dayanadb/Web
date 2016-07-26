95b5d6936835dd242385d88f7b7a070aab56503e
LocalAdm <LocalAdm>
2016-07-26 12:03:31 -0300

    locaos e iteracoes

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/lacos_e_iteracao.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/lacos_e_iteracao.js
@@ -1,4 +1,28 @@
-/**
- * 
- */
+console.log("LAÇO FOR")
+for (var a = 0; a < 10; a++){
+    console.log('a = ' + a);
+}
 
+console.log("LAÇO DO")
+var a_do = 10;
+do {
+    console.log('a = ' + a_do);
+    a_do++;
+} while(a_do < 10);
+
+console.log("LAÇO WHILE")
+var a_while = 0;
+while (a_while < 10) {
+    console.log('a = ' + a_while);
+    a_while++;
+}
+
+console.log("BREAK AND CONTINUE")
+for (var a = 0; a < 10; a++) {
+    if (a < 5) {
+        console.log('a < 5 --> continue');
+        continue;
+    }
+    console.log('a >= 5 --> break');
+    break;
+}
