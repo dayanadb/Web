d5a7cc4e7fded87cb3337b374349926ee905cdc0
LocalAdm <LocalAdm>
2016-07-26 09:44:22 -0300

    Adicionado linhas em sintaxe_e_tipos.js

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/sintaxe_e_tipos.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/sintaxe_e_tipos.js
@@ -1,4 +1,23 @@
-/**
- * 
+/*
+ Capítulo 1 - Javascript - Sintaxes e Tipos
+
+ Formas de se declarar variável:
+  _
+  a,b [0-9]
+  $
+
  */
 
+var a = true; //declaração de variável
+var b = 1;
+
+
+function teste() {
+    if (a === true) {
+        let c = false;
+        console.log(c);
+    } else {
+        const d = true;
+        console.log(d);
+    }
+}
