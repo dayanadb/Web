5666394804672e9ecd0cf2b6714cafed6da480f0
Renato Villas Boas Medeiros <rvmedeiros@scopus.com.br>
2016-07-26 14:29:49 -0300

    correção aula de  26/07

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/controle_de_fluxo_e_manipulacao_de_erro.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/controle_de_fluxo_e_manipulacao_de_erro.js
@@ -1,38 +1,40 @@
 /**
  * 
  */
-
 var a = 50;
-
-if(a == 50)
-{
-    try{
+if (a == 50) {
+    try {
         console.log("Estou aqui");
     }
-    catch(erro){
-        finally{
+    catch (erro) {
+        finally {
             console.log("Acabou");
         }
     }
 }
-else if(a == 12)
-{
+else if (a == 12) {
     console.log("Estou aqui")
 }
-else
-{
+else {
     a = 5 + 5;
 }
-
 switch (a > 20)
-        case 1: a = 4-8;
-        break;
+case 1:
+    a = 4 - 8;
+    break;
+case 2:
+    try {
+        a = 5 + 8;
+    }
+    catch (erro) {
+        console.log("Errou")
+    }
+default:
+    console.log("Estou aqui")
 
-        case 2: try{
-                a = 5+8;
+/**
+ * CONSIDERAÇÕES:
+    A instrução switch deve ser encapsulada com {}. Exemplo: switch (a){} (todos os cases devem estar dentro das chaves). O switch testará o valor dentro das clausulas case, desta forma, não testamos booleanos dentro do ().
 
-        }
-        catch(erro){
-            console.log("Errou")
-        }
-default:    console.log("Estou aqui")
+    No mais, está muito bom!
+  */
--- TreinamentoAppClient/WebContent/aluno/exercicios/js/lacos_e_iteracao.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/lacos_e_iteracao.js
@@ -3,30 +3,35 @@
  */
 //for
 var b = 10;
-for(var i = 0; i < b; i++)
-{
+for (var i = 0; i < b; i++) {
     console.log(i);
 }
 
 //do while
 var x = 5;
-do{
+do {
     x++;
 }
-while(x <5);
+while (x < 5);
 
 //while
 var d;
 var y = 20;
-while(y < 20){
+while (y < 20) {
     console.log("Teste");
     y++;
 }
 
 //for in
-var obj = {nome:"Ivan",idade:"21";
-var teste = "";
-for(teste in obj)
-{
-    console.log(teste);
-}
+var obj = {
+        nome: "Ivan"
+        , idade: "21";
+        var teste = "";
+        for (teste in obj) {
+            console.log(teste);
+        }
+
+/**
+ * CONSIDERAÇÕES:
+    Perfeito! Apenas não conseguiu criar o exercício para o for...of - caso tenha ficado alguma dúvida, me procure.
+  */
