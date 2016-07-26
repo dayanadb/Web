4fcce9876543312091b628d9c23f9e614fc4223e
LocalAdm <LocalAdm>
2016-07-26 09:48:42 -0300

    i

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/sintaxe_e_tipos.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/sintaxe_e_tipos.js
@@ -1,4 +1,38 @@
 /**
  * 
  */
+//comentário de linha simples
+/**Comentário
+ *de
+ *múltiplas
+ *linhas
+ */
+var var_Array = [10, true, "meu_nome"];
+var var_Boolean = false;
+var var_Inteiro = 4;
+var var_String = "oi";
+var var_Object = {
+    nome: "Marco"
+    , idade: 27
+    , barba = true
+};
 
+function funcaoTeste() {
+    var c = true;
+    if (c) {
+        let let_Array = [20, false, "sobrenome"];
+        let let_Boolean = true;
+        let let_Int = 4;
+        let let_String = "tchau";
+        let let_Object = {
+            raca: "cachorro"
+            , patas: 4
+            , focinho: true
+        }
+        const const_Array = [2, 3, 4];
+        const const_Boolean = false;
+        const const_Int = 2;
+        const const_String = "oi";
+        const
+    }
+}
