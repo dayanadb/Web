e3a8f7bfbc605d3a18496bbc9ad32ecdbb8441f1
Renato Villas Boas Medeiros <rvmedeiros@scopus.com.br>
2016-07-26 15:53:00 -0300

    correção aula 26/07

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/controle_de_fluxo_e_manipulacao_de_erro.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/controle_de_fluxo_e_manipulacao_de_erro.js
@@ -13,15 +13,19 @@ else {
     alert("Resultado 2");
 }
 
-
 switch (x) {
-    case (1):
+case (1):
     console.log("CASE 1");
     break;
-
-    case(12):
+case (12):
     console.log("Case 2");
     break;
-
-    default: console.log("Nenhuma das anteriores")
+default:
+    console.log("Nenhuma das anteriores")
 }
+
+/* CONSIDERAÇÕES:
+    Os parenteses dentro das clausulas 'case', são opcionais - poderia por exemplo escrever case 1, case 12
+    Percebi que não criou os exercícios de try...catch e throw. Caso tenha dúvidas sobre, me procure que podemos fazer um reforço individual!
+    No mais, excelente!
+ */
