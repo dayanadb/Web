81edbb94904fc91e63d0ce0013e87146893cc4a6
Renato Villas Boas Medeiros <rvmedeiros@scopus.com.br>
2016-07-26 16:00:49 -0300

    correção aula de  26/07

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/controle_de_fluxo_e_manipulacao_de_erro.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/controle_de_fluxo_e_manipulacao_de_erro.js
@@ -1,21 +1,25 @@
 /**
  * 
  */
-
-function ExcessaoUsuario(mensagem){
+function ExcessaoUsuario(mensagem) {
     this.erro = mensagem;
 }
 
-function testarErrosIf(parametro){
-
-    try{
-        if(parametro == 1){
+function testarErrosIf(parametro) {
+    try {
+        if (parametro == 1) {
             var excessao = new ExcessaoUsuario("Usuário Inválido");
             throw excessao;
-        }else{
+        }
+        else {
             alert("Executado com sucesso!");
         }
-    }catch(ExcessaoUsuario){
+    }
+    catch (ExcessaoUsuario) {
         alert(ExcessaoUsuario.erro);
     }
 }
+
+/* CONSIDERAÇÕES:
+    Perfeito! Apenas notei que não escreveu o exercício de switch. Caso tenha alguma duvida sobre como criar o switch, me procure!
+ */
--- TreinamentoAppClient/WebContent/aluno/exercicios/js/lacos_e_iteracao.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/lacos_e_iteracao.js
@@ -1,4 +1,4 @@
-/**
- * 
+/* CONSIDERAÇÕES:
+    Teve alguma dificuldade para fazer esse exercício? Caso sim, poderíamos fazer um reforço individual, só me procurar
  */
 
