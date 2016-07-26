458c8e9a8fa22f90b84a17575e577e859ee98938
Renato Villas Boas Medeiros <rvmedeiros@scopus.com.br>
2016-07-26 14:25:47 -0300

    correção aula de  26/07

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/controle_de_fluxo_e_manipulacao_de_erro.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/controle_de_fluxo_e_manipulacao_de_erro.js
@@ -2,28 +2,32 @@
  * Try, cacth, finally
  */
 var idade = 20;
-
-try{
+try {
     var novaIdade = calculo(idade);
     console.log(novaIdade);
-}catch (Error){
+}
+catch (Error) {
     console.log("Por favor, preencher sua idade");
- }
-
-
-
-switch(idade){
-    case 10: idade = idade +1; break;
-
-    default: idade; console.log(idade);
 }
 
-function calculo(idade){
-    if(idade != null ){
-       return idade = idade + 1;
-    }else {
+switch (idade) {
+case 10:
+    idade = idade + 1;
+    break;
+default:
+    idade;
+    console.log(idade);
+}
+
+function calculo(idade) {
+    if (idade != null) {
+        return idade = idade + 1;
+    }
+    else {
         throw Error;
     }
-
 }
-
+/**
+ * CONSIDERAÇÕES:
+    Perfeito, parabéns! Apenas verifique que na linha 27, o throw deve lançar um novo objeto do tipo Erro - poderia ajustar para ficar assim: throw new Error('idade está nula');
+  */
--- TreinamentoAppClient/WebContent/aluno/exercicios/js/lacos_e_iteracao.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/lacos_e_iteracao.js
@@ -1,4 +1,5 @@
 /**
- * 
- */
+ * CONSIDERAÇÕES:
+    Não conseguiu fazer o exercício devido a alguma dificuldade? Se sim, podemos fazer um reforço individual para que consiga fazer esse exercício.
+  */
 
