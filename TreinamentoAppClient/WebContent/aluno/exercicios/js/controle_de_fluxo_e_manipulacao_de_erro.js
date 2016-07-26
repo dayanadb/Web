ff26834f80601a57e794ca98767420e844bd443f
Renato Villas Boas Medeiros <rvmedeiros@scopus.com.br>
2016-07-26 15:57:32 -0300

    correção aula de  26/07

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/controle_de_fluxo_e_manipulacao_de_erro.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/controle_de_fluxo_e_manipulacao_de_erro.js
@@ -16,6 +16,7 @@ try {
     else {
         console.log("não é nem um nem dois");
     }
+
     switch (a)
 case 1:
     {
@@ -36,3 +37,10 @@ catch {
 finally {
     console.log("finalmente");
 }
+
+/* CONSIDERAÇÕES:
+    Na linha 10, else if deve ser separado ( colocou elseif, tudo junto)
+    No throw da linha 12, não seria obrigatorio declara um escopo de bloco para lançar o erro. Por exemplo, poderia fazer throw "Não deveria ser dois"
+    Nos cases das linhas 21 e 25 não declarou break no final dos cases  - o que acarretaria que cases seguintes também seriam executados. Caso tivesse sido essa a sua intenção ok! Apenas esteja atento!
+    No mais, muito bom!
+ */
--- TreinamentoAppClient/WebContent/aluno/exercicios/js/lacos_e_iteracao.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/lacos_e_iteracao.js
@@ -4,16 +4,19 @@
 for (var x = 1, x < 10, x++) {
     console.log("for loop x = " + x);
 }
+
 var a = 1;
 do {
     console.log("do/while loop a = " + a);
     a++;
 } while (a < 10);
+
 a = 0;
 while (a < 10); {
     console.log("while loop a = " + a);
     a++;
 }
+
 var b = 0;
 while (a < 10); {
     console.log("while break loop b = " + b);
@@ -22,6 +25,7 @@ while (a < 10); {
         break;
     }
 }
+
 console.log("while break loop b final = " + b);
 
 var b = 0;
@@ -32,12 +36,17 @@ while (a < 10); {
         continue;
     }
 }
+
 console.log("while continue loop b final = " + b);
 
 /* falta for in
     e
     for of
 */
+
+/* CONSIDERAÇÕES:
+    Muito bom, entretando senti a falta dos exercícios de for...in e for...of. Teve alguma dificuldade para fazer esses? Caso sim, poderíamos fazer um reforço individual, só me procurar
+ */
 
 
 
