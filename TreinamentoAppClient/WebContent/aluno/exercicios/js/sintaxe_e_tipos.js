eff6be5f1a62370e7f637d54e4f4818e83db8259
LocalAdm <LocalAdm>
2016-07-26 10:23:57 -0300

    teste js

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/sintaxe_e_tipos.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/sintaxe_e_tipos.js
@@ -5,6 +5,32 @@ function teste()
     /* Vamos testar de volta
        esse exercicio*/
 
-    const teste = 2;
+    var a = ["Saab", "Volvo", "BMW"];
+    var b = true;
+    var c = 10;
+    var d = "teste";
+    var e = {type:"Fiat", model:"500", color:"white"};
 
+    // let var1 [= ["Saaib", "Volvo", "BMW"]] [, var2 [= ["Saiiab", "Volvo", "BMW"]]];
+    // let var3 [= true] [, var4 [= false]];
+    //let var5 [= 10] [, var6 [= 30]];
+    //let var7 [= "testando"] [, var8 [= "valor"]];
+    //let var8 [= {type:"volvo", model:"500", color:"white"};] [, var9 [= {type:"modelo", //model:"500", color:"white"};]];
+
+    //const name1 = ["Saab", "Volvo", "BMW"] [, name2 = ["Saab", "Volvo", "BMW"]];
+    //const name3 = true [, name4 = false ];
+    //const name4 = 110 [, name5 = 150 ];
+    //const name6 = "Um" [, name7 = "dois" ];
+    //const name8 = {type:"volvo", color:"white"} [, name9 = {type:"volvo", color:"white"}];
+
+    console.log(a,b,c,d,e);
 }
+
+function testando()
+{
+    var c = 10+10;
+    console.log(c);
+}
+
+console.log(teste());
+console.log(testando());
