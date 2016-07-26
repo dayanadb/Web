0f2c10a8f7cd430abfc098dda5965c05524f1c53
Wesley Calesso Arantes <wcarantes@scopus.com.br>
2016-07-26 12:04:00 -0300

    JavaScript

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/controle_de_fluxo_e_manipulacao_de_erro.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/controle_de_fluxo_e_manipulacao_de_erro.js
@@ -2,3 +2,20 @@
  * 
  */
 
+function ExcessaoUsuario(mensagem){
+    this.erro = mensagem;
+}
+
+function testarErrosIf(parametro){
+
+    try{
+        if(parametro == 1){
+            var excessao = new ExcessaoUsuario("Usuário Inválido");
+            throw excessao;
+        }else{
+            alert("Executado com sucesso!");
+        }
+    }catch(ExcessaoUsuario){
+        alert(ExcessaoUsuario.erro);
+    }
+}
--- TreinamentoAppClient/WebContent/aluno/exercicios/js/sintaxe_e_tipos.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/sintaxe_e_tipos.js
@@ -1,4 +1,50 @@
-/**
- * 
- */
+/*
+    Exercício Wesley
+    Declarar uma variável de cada tipo para todas as declarações
+*/
 
+// Escopo Globa
+var booleana;
+var nula;
+var indefinida;
+var numero;
+var texto;
+var simbolo;
+var objeto;
+
+function inicializacaoVariaveis() {
+    //Escopo Local
+    booleana = true;
+    nula = null;
+    numero = 10;
+    texto = "Texto";
+    simbolo = Symbol("Símbolo");
+    objeto = {nome: "Wesley",idade: 28};
+
+    console.log("Variáveis inicializadas");
+}
+
+function imprimirVariaves(){
+    console.log(booleana);
+    console.log(nula);
+    console.log(numero);
+    console.log(texto);
+    console.log(simbolo);
+    console.log(objeto);
+}
+
+function usarLetConst(opcao){
+    if(opcao == 1){
+        let nome="Wesley";
+        const empresa="Scopus";
+
+        console.log(nome);
+        console.log(empresa);
+    }else{
+        let nome="Outra pessoa";
+        const empresa="Outra empresa";
+
+        console.log(nome);
+        console.log(empresa);
+    }
+}
