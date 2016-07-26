d0d930afe1b35951c0e1ffef66a3f4bca61c5bce
Ricardo Pinto Ramos <rpramos@scopus.com.br>
2016-07-26 10:57:43 -0300

    Switch / Case

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/controle_de_fluxo_e_manipulacao_de_erro.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/controle_de_fluxo_e_manipulacao_de_erro.js
@@ -1,16 +1,27 @@
 /**
  * 
  */
-
 var b = 10;
-var a = 2;
+var a = 1;
 var x;
-
 x = a + b;
-    if (x > 12) {
-        console.log(x);
-      }else {
-        console.log(a);
-        console.log(b);
-        }
+if (x > 12) {
+    alert(x);
+}
+else {
+    alert("Resultado 1");
+    alert("Resultado 2");
+}
 
+
+switch (x) {
+    case (1):
+    console.log("CASE 1");
+    break;
+
+    case(12):
+    console.log("Case 2");
+    break;
+
+    default: console.log("Nenhuma das anteriores")
+}
