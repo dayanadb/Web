3fee4e2541a5c09b6202979339ae2e2feb65f4f5
Ivan Moleta <imoleta@scopus.com.br>
2016-07-26 12:11:41 -0300

    ajuste

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/lacos_e_iteracao.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/lacos_e_iteracao.js
@@ -1,4 +1,32 @@
 /**
  * 
  */
+//for
+var b = 10;
+for(var i = 0; i < b; i++)
+{
+    console.log(i);
+}
 
+//do while
+var x = 5;
+do{
+    x++;
+}
+while(x <5);
+
+//while
+var d;
+var y = 20;
+while(y < 20){
+    console.log("Teste");
+    y++;
+}
+
+//for in
+var obj = {nome:"Ivan",idade:"21";
+var teste = "";
+for(teste in obj)
+{
+    console.log(teste);
+}
