8f1dc628b73681df60273e9fb70481937977bfa8
Ricardo Pinto Ramos <rpramos@scopus.com.br>
2016-07-26 09:56:32 -0300

    Exercicio1 - Sintaxe e Tipos

---
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/Testes.js
@@ -0,0 +1,14 @@
+function(alertTela){
+    var teste;
+    var fazer;
+    var b = {nome:Ricardo, enereco:Teixeira, numero: 10};
+
+    teste = false;
+
+    while(teste != true) {
+        fazer +1;
+        alert(fazer);
+        teste=true;
+    }
+
+}
--- TreinamentoAppClient/WebContent/aluno/exercicios/js/sintaxe_e_tipos.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/sintaxe_e_tipos.js
@@ -1,4 +1,27 @@
-/**
- * 
- */
+// Este é um comentario de linha simples
+/* Este é um comentario
+de
+linhas
+multiplas
+*/
 
+var vArray = ["EDM", "AXE","funk"] ;
+var vBoolean = true;
+var vInt = 10;
+var vString = "String de Texto"
+var vObject = {nome:"Ricardo",endereco:"Desembargador",numero:10};
+
+let lArray = ["laranja", "banana"]
+let lBoolean = true;
+let lInt = 21;
+let lString = "String de Let";
+let lObject = {nome:"Renato", endereco:2, numero:12};
+const cArray = ["cimento", "areia"];
+const cBoolean = false;
+const cInt = 28;
+const cString ="String para Constante";
+const cObject = {nome:"Renato", endereco:2, numero:12};
+
+console.log(vArray);
+console.log(lBoolean);
+console.log(cInt);
