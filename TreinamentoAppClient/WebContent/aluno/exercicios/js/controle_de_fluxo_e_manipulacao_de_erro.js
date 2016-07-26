174d4dcb76959a8275e8e9b9b7ebb545d340d628
LocalAdm <LocalAdm>
2016-07-26 11:51:53 -0300

    js

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/controle_de_fluxo_e_manipulacao_de_erro.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/controle_de_fluxo_e_manipulacao_de_erro.js
@@ -14,17 +14,31 @@ function testaIf()
 
 function caso()
 {
+   var day;
     switch (new Date().getDay())
     {
-        case 6:
-            console.log = ("Today is Saturday");
+        case 0:
+            day = "Sunday";
         break;
-            case 0:
-        console.log = ("Today is Sunday");
+        case 1:
+            day = "Monday";
         break;
-        default:
-        console.log = ("Looking forward to the Weekend");
+        case 2:
+            day = "Tuesday";
+        break;
+        case 3:
+            day = "Wednesday";
+        break;
+        case 4:
+            day = "Thursday";
+        break;
+        case 5:
+            day = "Friday";
+        break;
+        case  6:
+            day = "Saturday";
     }
+    console.log(day);
 }
 
 console.log(testaIf());
