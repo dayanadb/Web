41f602101fe4314790f3d6b58ea8c5c03a85d237
LocalAdm <LocalAdm>
2016-07-26 11:29:42 -0300

    release

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/sintaxe_e_tipos.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/sintaxe_e_tipos.js
@@ -1,14 +1,24 @@
-/**
+/*
  Teste de realização de comentário de bloco
  */
 // declaração de variáveis
 var $a = 10;
 var $b = "teste";
 var $c = [10, "teste", false];
+var car = {type:"Fiat", model:"500", color:"white"};
+teste();
+console.log("var $a= " + $a);
+console.log("var $b= " + $b);
+console.log("var $c= " + $c);
+console.log("var car= " + car);
 
 function teste() {
-    var x = "testes";
-    let y = "var de bloco";
-    let z = true;
-    let x = 10.10;
+    var _x = "testes";
+    let _y = "var de bloco";
+    let _z = true;
+    let _w = 10.10;
+    console.log("var x= " + _x);
+    console.log("var y= " + _y);
+    console.log("var z= " + _z);
+    console.log("var w= " + _w);
 }
