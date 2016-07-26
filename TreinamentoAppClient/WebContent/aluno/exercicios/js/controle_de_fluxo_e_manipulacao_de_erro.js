af78f8ef8ff83040802a80931771e38c467f389b
LocalAdm <LocalAdm>
2016-07-26 11:30:26 -0300

    Controle de fluxo e Manipulação de erros

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/controle_de_fluxo_e_manipulacao_de_erro.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/controle_de_fluxo_e_manipulacao_de_erro.js
@@ -2,3 +2,17 @@
  * 
  */
 
+function validateLogin() {
+    var usuario = document.getElementById('usuario');
+
+    if (usuario === null) {
+        throw new Error("Campo usuario não definido na tela");
+    }
+
+    try {
+        usuario.value = 'teste';
+    }
+    catch(err) {
+        conso.log(err.message);
+    }
+}
