f7b3de6a584f87886374b2e22da14a6d156cdee6
LocalAdm <LocalAdm>
2016-07-26 12:03:53 -0300

    js

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/lacos_e_iteracao.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/lacos_e_iteracao.js
@@ -1,4 +1,56 @@
-/**
- * 
- */
+function primeiro()
+{
+    var text;
 
+    for (var i = 0; i<=3;i++)
+    {
+        text+="Ola";
+    }
+
+    console.log(text)
+}
+
+function segundo()
+(
+    var text;
+    do
+    {
+        text += "O numero é: " + i;
+        i++;
+    }
+    while (i < 10);
+
+    console.log(text)
+)
+
+function terceiro()
+{
+    var numero;
+
+    while (var i < 10)
+    {
+        numero += "O numero é " + i;
+        i++;
+    }
+
+    console.log(numero);
+}
+
+function quarto()
+{
+    var i;
+
+    while(i <= 5)
+    {
+        if(i ==3)
+        {
+            console.log("Parei");
+        }
+        break;
+    }
+}
+
+function quinto()
+{
+
+}
