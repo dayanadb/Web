69297f717a182dc3df6b98ad8d577f1efd5d5dfb
LocalAdm <LocalAdm>
2016-07-26 11:51:42 -0300

    k

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/controle_de_fluxo_e_manipulacao_de_erro.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/controle_de_fluxo_e_manipulacao_de_erro.js
@@ -1,4 +1,32 @@
 /**
  * 
  */
+var a = 6;
+var resultado_if;
+var resultado_switch;
 
+if (a > 0 && a <= 3) {
+    resultado_if = "Situação A";
+}
+else if (a > 3 && a < 6) {
+    resultado_if = "Situação B";
+}
+else {
+    resultado_if = "Situação C";
+}
+console.log(resultado_if);
+var string_switch = "Talvez";
+switch (string_switch) {
+case "Sim":
+    resultado_switch = "Mis";
+    break;
+case "Não":
+    resultado_switch = "Oãn";
+    break;
+case "Talvez":
+    resultado_switch = "Zevlat";
+    break;
+default:
+    resultado_switch = "none";
+}
+console.log(resultado_switch);
