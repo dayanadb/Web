dc50c0a759685a5b8f49cbaee774e90c24c16391
LocalAdm <LocalAdm>
2016-07-26 11:58:01 -0300

    terça20160726

--- TreinamentoAppClient/WebContent/aluno/avaliacao/js/controle_de_fluxo_e_manipulacao_de_erro.js
+++ TreinamentoAppClient/WebContent/aluno/avaliacao/js/controle_de_fluxo_e_manipulacao_de_erro.js
@@ -1,4 +1,21 @@
-/**
- * 
- */
 
+function verificaNumero(num)
+{
+	var num = 1000;
+	var idade = num;
+	if( num==1000)
+	{
+		try
+		{
+			idade = num / idade;
+		}
+		catch
+		{
+			console.error("idade não existe");
+		}
+	}
+	else trow
+	{
+		console.log("fim de jogo")
+	}
+};
--- TreinamentoAppClient/WebContent/aluno/avaliacao/js/lacos_e_iteracao.js
+++ TreinamentoAppClient/WebContent/aluno/avaliacao/js/lacos_e_iteracao.js
@@ -1,4 +1,29 @@
-/**
- * 
- */
+var num = 100;
+
+for(i = 0; num < 100; i++)
+{
+	//todo
+}
+
+do
+{
+	//todo
+}
+while(num< 100)
+
+
+while(num < 100 )
+{
+	//todo
+}
+
+var pessoa = {nome:"Aldo", sobrenome:"Leonardo", idade:43};
+
+var text = "";
+var i;
+for (i in pessoa)
+{
+    text += pessoa[i];
+}
+
 
