afdf3c50d1d7937a0a671c0e776d8b105f3ea828
LocalAdm <LocalAdm>
2016-07-26 11:58:58 -0300

    javascript introdution

--- TreinamentoAppClient/WebContent/aluno/avaliacao/js/controle_de_fluxo_e_manipulacao_de_erro.js
+++ TreinamentoAppClient/WebContent/aluno/avaliacao/js/controle_de_fluxo_e_manipulacao_de_erro.js
@@ -1,4 +1,62 @@
 /**
  * 
  */
+var a = false
+    , b = 10
+    , c = "Joao"
+    , obj;
 
+function criaIfElse() {
+    if (a == true) {
+        console.log("ta tranquilo, ta favorável");
+    }
+    else throw Error();
+}
+
+function criaSwitch(i) {
+    switch (i) {
+    case 0:
+        a = i;
+    case 1:
+        a = i;
+    case 2:
+        a = i;
+    case 3:
+        a = i;
+    case 4:
+        a = i;
+    case 5:
+        a = i;
+    case 6:
+        a = i;
+    case 7:
+        a = i;
+    case 8:
+        a = i;
+    case 9:
+        a = i;
+    }
+    console.log(a);
+}
+
+function criaObjeto() {
+    obj = {
+        nome: "A"
+        , idade: 18
+        , sexo: "F"
+    }
+}
+try {
+    criaIfElse();
+}
+catch (Error) {
+    console.log(Error);
+    for (var i = 0; i < 10; i++) {
+        criaSwitch(i);
+    }
+}
+
+criaObjeto();
+for (var o in obj){
+    console.log(obj[o]);
+}
--- TreinamentoAppClient/WebContent/aluno/avaliacao/js/sintaxe_e_tipos.js
+++ TreinamentoAppClient/WebContent/aluno/avaliacao/js/sintaxe_e_tipos.js
@@ -1,4 +1,26 @@
-/**
- * 
- */
+//Comentário linha simples
+/*
+comentário
+linhas
+multiplas
+*/
+
+var a = [1, 2, 3, 4, 5], b = true, c = 11, d = "String", e = {chave : "valor", chave2 : "valor2"};
+
+
+
+function teste() {
+    if ( b == false ){
+        const a = [3, 2, 1, 5, 4], b = true, c = 99, d = "HUEHUE", e = {chave : "valordoconst", chave2 : "valordocontra"};
+    }
+
+    else if ( b == true ){
+        let a = [5, 4, 3, 2, 1], b = false, c = 24, d = "STRENGHUE", e = {chave : "valorerrado", chave2 : "valorzuado"};
+        console.log(a, b, c, d, e);
+    }
+}
+
+teste();
+console.log();
+
 
