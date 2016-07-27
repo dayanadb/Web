b46181d2f9585952b53ef1833e8fa08c2d4244c7
Scopus <Scopus>
2016-07-27 09:17:06 -0300

    laços e iteração

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/lacos_e_iteracao.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/lacos_e_iteracao.js
@@ -1,5 +1,73 @@
-/**
- * CONSIDERAÇÕES:
-    Talvés esteja com dificuldades e por consequencia não fez os exercícios. Vamos fazer um reforço individual para voce não ficar desestimulada? Me procure que fazemos em algum horário à tarde.
-  */
+function diaSemana(dia){
+    switch (dia) {
+        case 0:
+            dia = alert("Domingo");
+            break;
+        case 1:
+            dia = alert("Segunda-feira");
+            break;
+        case 2:
+            dia = alert("Terça-feira");
+            break;
+        case 3:
+            dia = alert("Quarta-feira");
+            break;
+        case 4:
+            dia = alert("Quinta-feira");
+            break;
+        case 5:
+            dia = alert("Sexta-feira");
+            break;
+        case 6:
+            dia = alert("Sábado");
+            break;
+        default:
+            dia = alert("Erro");
+    }
+}
+
+function imprimirPares()
+{
+    var i=1;
+
+    for (i=1; i<=20; i++)
+    {
+        if (i%2 == 0)
+        {
+            console.log(i);
+        }
+    }
+}
+
+
+function maior10()
+{
+    var x;
+    do
+    {
+        x = prompt("Informe um numero: ");
+        console.log(x);
+    }
+    while(x < 10)
+
+    alert("Maior que 10");
+}
+
+/*
+    throw
+
+    try
+    {
+        codigo
+    }
+    catch(exception)
+    {
+        //captura de erros
+    }
+    finally
+    {
+        //liberar uma conexao com BD
+        //fechar arquivo
+    }
+            */
 
