e7e16bcb7507731a3ab3c043fe55194cd0d40fc2
Ricardo Pinto Ramos <rpramos@scopus.com.br>
2016-07-27 10:50:53 -0300

    Exercicios de Funcoes

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/funcoes.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/funcoes.js
@@ -1,4 +1,39 @@
-/**
- * 
- */
+//Funcao anonima
+
+var anonima = function (){
+    return null;
+}
+// Funcao não Anonima - retorna String
+function nAnonima(){
+    var st = "String Literal"
+    return st;
+}
+//Variavel com escopo de Funcao;
+var abc = nAnonima;
+
+
+//Funcao aninhada
+function aninhada (){
+    var xyz;
+    function aninhada2() {
+        xyz = 10;
+        return xyz * 10;
+    }
+}
+
+//Funcao com parametro padrão
+function idadeAno(x,y){
+
+}
+
+//Funcao Parametro Rest
+function typeRest (a,b, ...Args) {
+    return null;
+
+}
+// Funcao com uso de "this"
+function typeThis(){
+    return this.a + this.c;
+}
+
 
