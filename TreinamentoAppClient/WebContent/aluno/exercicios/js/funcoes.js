387fb0e6f38294877933458b34e792cf4636ab2f
LocalAdm <LocalAdm>
2016-07-27 11:26:21 -0300

    funcoes

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/funcoes.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/funcoes.js
@@ -1,4 +1,42 @@
-/**
- * 
- */
+//Função não anonima sem retorno
+var a;
 
+function FA() {
+    var b = 1;
+    a = b;
+}
+//Função não anonima com retorno String Literal
+function fb() {
+    var b = 2;
+    a = b;
+    return "literal";
+}
+//variavel com o escopo da primeira função
+var c = FA();
+//função aninhada
+function fc() {
+    var b = 1;
+    a = b;
+
+    function fd() {
+        var c = 2;
+        a = b;
+        return a;
+    }
+}
+//função com ao menos um parametro padrão
+function fe(x){
+    return x;
+}
+//função com um parametro rest
+function ff(y, ...z){
+    console.log(y);
+    console.log(z.length);
+}
+//função com this
+function fg(x){
+    this.id = x;
+}
+
+var h = new fg("01");
+console.log(h.id);
