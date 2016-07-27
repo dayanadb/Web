9fb42018dcd6708272e6bcef75fe4368fa263a8a
Scopus <Scopus>
2016-07-27 08:48:08 -0300

    js

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/controle_de_fluxo_e_manipulacao_de_erro.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/controle_de_fluxo_e_manipulacao_de_erro.js
@@ -1,4 +1,88 @@
-/**
- * 
- */
+function ola()
+{
+    var nome = "Anne";
+
+    if (nome == "Anne")
+    {
+        alert("Olá Anne!");
+    }
+    else
+    {
+        alert("Olá");
+    }
+}
+
+
+function diaSemana(dia){
+    switch (dia) {
+        case 0:
+            dia = alert("Domingo");
+            break;
+        case 1:
+            dia = alert("Segunda-feira");
+            break;
+        case 2:
+            dia = alert("Terça-feira");
+            break;
+        case 3:
+            dia = alert("Quarta-feira");
+            break;
+        case 4:
+            dia = alert("Quinta-feira");
+            break;
+        case 5:
+            dia = alert("Sexta-feira");
+            break;
+        case 6:
+            dia = alert("Sábado");
+            break;
+        default:
+            dia = alert("Erro");
+    }
+}
+
+function imprimirPares()
+{
+    var i=1;
+
+    for (i=1; i<=20; i++)
+    {
+        if (i%2 == 0)
+        {
+            console.log(i);
+        }
+    }
+}
+
+
+function maior10()
+{
+    var x;
+    do
+    {
+        x = prompt("Informe um numero: ");
+        console.log(x);
+    }
+    while(x < 10)
+
+    alert("Maior que 10");
+}
+
+/*
+    throw
+
+    try
+    {
+        codigo
+    }
+    catch(exception)
+    {
+        //captura de erros
+    }
+    finally
+    {
+        //liberar uma conexao com BD
+        //fechar arquivo
+    }
+            */
 
--- TreinamentoAppClient/WebContent/aluno/exercicios/js/sintaxe_e_tipos.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/sintaxe_e_tipos.js
@@ -1,4 +1,40 @@
-/**
- * 
- */
+/*
 
+1º exercício:
+Criar variáveis
+
+*/
+
+
+//Escopo Global
+
+var booleana;
+var nula;
+var indefinida;
+var texto;
+var numero;
+var simbolo;
+var objeto;
+
+function initVar() {
+    booleana = true;
+    nula = null;
+    indefinida = undefined;
+    texto = "Texto";
+    numero = 0;
+//    simbolo = new Symbol("Simbolo");
+    objeto = {nome: "Annelisa", idade: 37};
+}
+
+function imprimirVar() {
+    console.log(booleana);
+    console.log(nula);
+    console.log(indefinida);
+    console.log(texto);
+    console.log(numero);
+    console.log(simbolo);
+    console.log(objeto);
+}
+
+initVar();
+imprimirVar();
