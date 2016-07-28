2a90aa1b62982ca07e3206bb9147c0f5770675d9
Roberto Carlos Leonardo <rcleonardo@scopus.com.br>
2016-07-28 09:46:50 -0300

    release

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/colecoes_indexadas.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/colecoes_indexadas.js
@@ -1,4 +1,55 @@
 /**
  * 
  */
+/* exemplos do professor
+var a = new array();
+var b = array();
+var c = [10, rue, {
+    nome: "x"
+}];
+var d = [][];
+a[0] = "x";
+d[0][0] = "teste";
+for (var a of b) {};
+b.foreach(function (x)) {
+          x...
+          };
+
+c.push(10); // cria a próixima posição a partir do último..
+a = a.concat(1,false,{});
+
+a.spice(1,2,5,"a");
+a.reverse();
+a.sort();
+
+*/
+// exercícios
+var x = Array(1, 2, 3, );
+
+var a = new Array("a", "b", "c");
+console.log("tamanho do array= " + a.length);
+
+
+var b = array();
+b[0] = "teste 1";
+
+var c = array();
+c[0] = "teste 1";
+c[1] = "teste 2";
+c[2] = "teste 3";
+c.forEach(function (x)) {
+          console.log("posicao= " + c[x]);
+};
+
+var d=[][];
+d[0][0] = "teste 00";
+d[0][1] = "teste 01";
+d[1][0] = "teste 10";
+d[1][1] = "teste 11";
+
+for (x=0;x<2;x++) {
+    for (y=0;y<2;y++) {
+        console.log("matriz = " + d[x][y]);
+    }
+}
 
