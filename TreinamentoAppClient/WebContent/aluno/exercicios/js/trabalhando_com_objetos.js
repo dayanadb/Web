0eb080c8a6ada041e78a96a981fdb32af2796e10
Scopus <Scopus>
2016-07-28 12:16:20 -0300

    update

--- TreinamentoAppClient/WebContent/aluno/exercicios/js/trabalhando_com_objetos.js
+++ TreinamentoAppClient/WebContent/aluno/exercicios/js/trabalhando_com_objetos.js
@@ -1,4 +1,17 @@
 /**
- * 
+ * Trablahndo com objetos
  */
+
+var obj = { nome:"Nome",tag:"tag",func: function(){}};
+
+console.log(obj);
+
+function Pessoa(nome,idade){
+    this.nome = nome;
+    this.idade = idade;
+}
+
+var pessoa = new Pessoa("Lucas",23);
+
+console.log(pessoa);
 
