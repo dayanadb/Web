a10e1b1a990489b74903f9af65d185ba31303f2f
Renato Villas Boas Medeiros <rvmedeiros@scopus.com.br>
2016-08-01 15:35:31 -0300

    avaliação

--- TreinamentoAppClient/WebContent/aluno/avaliacao/js/funcoes.js
+++ TreinamentoAppClient/WebContent/aluno/avaliacao/js/funcoes.js
@@ -1,4 +1,5 @@
-/**
- * 
- */
+/*1) Declare uma função não-anonima que retorne 'bem-vindo'
+2) Declare uma função anonima (armazenando o escopo dentro de uma variavel), sem retorno
+3) DEclare uma função (anonima ou não-anonima) e dentro desta declare outro função 'interna'. Faça com que a função mais externa retorne a mais interna*/
+
 
