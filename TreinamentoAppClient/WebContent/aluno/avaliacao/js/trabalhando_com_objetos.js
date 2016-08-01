f45cd5aa17500b3fcc67ba6d54ce3abfe10787d8
Leticia Gusmao Dolny <ldolny@scopus.com.br>
2016-08-01 16:11:55 -0300

    teste

--- TreinamentoAppClient/WebContent/aluno/avaliacao/js/trabalhando_com_objetos.js
+++ TreinamentoAppClient/WebContent/aluno/avaliacao/js/trabalhando_com_objetos.js
@@ -1,3 +1,4 @@
 /*Declare um tipo através de função, contendo dois atributos internos chamado 'nome' e 'treinamento'. Deixá-los undefined
 Crie um objeto a partir da declaração acima, utilizando alguma das duas formas possível para instanciação*/
 
+sdsd
