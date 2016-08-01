497c9c106ffc9d1d1c7ff2f3c5dd254ad300f5ec
Renato Villas Boas Medeiros <rvmedeiros@scopus.com.br>
2016-08-01 14:34:34 -0300

    avaliação

--- TreinamentoAppClient/WebContent/aluno/avaliacao/js/sintaxe_e_tipos.js
+++ TreinamentoAppClient/WebContent/aluno/avaliacao/js/sintaxe_e_tipos.js
@@ -1,26 +1,6 @@
-//Comentário linha simples
 /*
-comentário
-linhas
-multiplas
+1) Declare uma variavel (undefined)
+2) Inicialize a variavel acima com um literal 'Number'
+3) Declare uma variável de escopo de bloco
 */
-
-var a = [1, 2, 3, 4, 5], b = true, c = 11, d = "String", e = {chave : "valor", chave2 : "valor2"};
-
-
-
-function teste() {
-    if ( b == false ){
-        const a = [3, 2, 1, 5, 4], b = true, c = 99, d = "HUEHUE", e = {chave : "valordoconst", chave2 : "valordocontra"};
-    }
-
-    else if ( b == true ){
-        let a = [5, 4, 3, 2, 1], b = false, c = 24, d = "STRENGHUE", e = {chave : "valorerrado", chave2 : "valorzuado"};
-        console.log(a, b, c, d, e);
-    }
-}
-
-teste();
-console.log();
-
 
