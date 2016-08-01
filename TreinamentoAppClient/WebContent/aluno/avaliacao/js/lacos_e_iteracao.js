e7551fedf2050fc9a06d66c7df1118447582dda4
Renato Villas Boas Medeiros <rvmedeiros@scopus.com.br>
2016-08-01 14:43:55 -0300

    avaliação

--- TreinamentoAppClient/WebContent/aluno/avaliacao/js/lacos_e_iteracao.js
+++ TreinamentoAppClient/WebContent/aluno/avaliacao/js/lacos_e_iteracao.js
@@ -1,4 +1,4 @@
-/**
- * 
- */
+/*1) Crie um laço que teste uma condição antes de entrar no loop
+2) Dado o objeto {nome: "seu_nome", treinamento: "AWC"}, utilize a estrutura de repetição apropriada para imprimir no console os nomes dos atributos do objeto acima
+3) Crie um laço 'for' que itere nos valores entre 5 e 20, imprimindo no console o valor corrente da iteração*/
 
--- TreinamentoAppClient/WebContent/aluno/avaliacao/js/sintaxe_e_tipos.js
+++ TreinamentoAppClient/WebContent/aluno/avaliacao/js/sintaxe_e_tipos.js
@@ -1,4 +1,6 @@
-/**
- * 
- */
+/*
+1) Declare uma variavel (undefined)
+2) Inicialize a variavel acima com um literal 'Number'
+3) Declare uma variável de escopo de bloco
+*/
 
