72ebed3aa92f227eb51ed0cff341beaae880a29b
Renato Villas Boas Medeiros <rvmedeiros@scopus.com.br>
2016-08-01 14:26:41 -0300

    avaliação

--- TreinamentoAppClient/WebContent/aluno/avaliacao/js/trabalhando_com_objetos.js
+++ TreinamentoAppClient/WebContent/aluno/avaliacao/js/trabalhando_com_objetos.js
@@ -1,4 +1,3 @@
-/**
- * 
- */
+/*Declare um tipo através de função, contendo dois atributos internos chamado 'nome' e 'treinamento'. Deixá-los undefined
+Crie um objeto a partir da declaração acima, utilizando alguma das duas formas possível para instanciação*/
 
