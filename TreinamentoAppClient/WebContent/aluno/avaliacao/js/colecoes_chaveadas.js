76d3c7f40142741979b0097e6f36a76d0994fb67
Renato Villas Boas Medeiros <rvmedeiros@scopus.com.br>
2016-08-01 14:32:23 -0300

    avaliação

--- TreinamentoAppClient/WebContent/aluno/avaliacao/js/colecoes_chaveadas.js
+++ TreinamentoAppClient/WebContent/aluno/avaliacao/js/colecoes_chaveadas.js
@@ -1,4 +1,2 @@
-/**
- * 
- */
-
+// 1) Crie uma coleção do tipo chaveada que não permita duplicidade de elementos
+// 2) Crie uma coleção do tipo chaveada que utiliza o par "chave/valor" para iteração
--- TreinamentoAppClient/WebContent/aluno/avaliacao/js/trabalhando_com_objetos.js
+++ TreinamentoAppClient/WebContent/aluno/avaliacao/js/trabalhando_com_objetos.js
@@ -1,4 +1,5 @@
-/**
- * 
- */
+/*
+1) Declare um tipo através de função, contendo dois atributos internos chamado 'nome' e 'treinamento'. Deixá-los undefined
+2) Crie um objeto a partir da declaração acima, utilizando alguma das duas formas possível para instanciação
+*/
 
