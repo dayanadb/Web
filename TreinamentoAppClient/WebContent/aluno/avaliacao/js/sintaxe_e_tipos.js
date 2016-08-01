259a7a79fea4787ef6c94da83b2f387572ceb90b
Renato Villas Boas Medeiros <rvmedeiros@scopus.com.br>
2016-08-01 14:29:54 -0300

    avaliação

--- TreinamentoAppClient/WebContent/aluno/avaliacao/js/sintaxe_e_tipos.js
+++ TreinamentoAppClient/WebContent/aluno/avaliacao/js/sintaxe_e_tipos.js
@@ -1,4 +1,5 @@
-/**
- * 
- */
+/*1) Declare uma variável, sem inicializá-la (undefined)
+2) Instancia a variável declarada acima com um literal do tipo 'String'
+3) Declare uma variável de escopo de bloco*/
+
 
--- TreinamentoAppClient/WebContent/aluno/avaliacao/js/trabalhando_com_objetos.js
+++ TreinamentoAppClient/WebContent/aluno/avaliacao/js/trabalhando_com_objetos.js
@@ -1,4 +1,3 @@
-/**
- * 
- */
+/*1) Declare um tipo atraves de uma função
+2) Instancia um objeto para o tipo definido acima, utilizando uma das duas formas possiveis de instanciação*/
 
